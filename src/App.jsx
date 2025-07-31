26f6423bce474cb7a355a183033218434071ed41
Irina <weedfox053@gmail.com>
2025-07-31 14:32:57 +0300

    React list of goods

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -14,33 +15,57 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
-export const App = () => (
+export const App = () => {
+  const [sortGoods, setGoods] = useState([...goodsFromServer]);
+  const [active, setActive] = useState('');
+  const sort = () => {
+ setGoods([...sortGoods].sort((a,b) => a.localeCompare(b)))
+ setActive('Sort alphabetically');
+ }
+
+const sortByLength = () => {
+ setGoods([...sortGoods].sort((a, b) => a.length - b.length))
+ setActive('Sort by length')
+}
+
+const reverse = () => {
+  setGoods([...sortGoods].reverse())
+  setActive('Reverse')
+}
+
+const reset = () => {
+  setGoods([...goodsFromServer])
+  setActive('')
+}
+
+return (
   <div className="section content">
     <div className="buttons">
-      <button type="button" className="button is-info is-light">
+      <button type="button" onClick={sort} className={`button is-info ${active == 'Sort alphabetically' ? '' : 'is-light'}`}>
         Sort alphabetically
       </button>
 
-      <button type="button" className="button is-success is-light">
+      <button type="button" onClick={sortByLength} className={`button is-success ${active == 'Sort by length' ? '' : 'is-light'}`}>
         Sort by length
       </button>
 
-      <button type="button" className="button is-warning is-light">
+      <button type="button" onClick={reverse} className={`button is-warning ${active == 'Reverse' ? '' : 'is-light'}`}>
         Reverse
       </button>
 
-      <button type="button" className="button is-danger is-light">
+      <button type="button"  onClick={reset} className="button is-danger is-light">
         Reset
       </button>
     </div>
 
     <ul>
-      <li data-cy="Good">Dumplings</li>
-      <li data-cy="Good">Carrot</li>
-      <li data-cy="Good">Eggs</li>
-      <li data-cy="Good">Ice cream</li>
-      <li data-cy="Good">Apple</li>
-      <li data-cy="Good">...</li>
+      {sortGoods.map((good, index) =>
+      <li data-cy="Good"
+      key={index}
+      >{good}</li>
+    )}
     </ul>
   </div>
 );
+};
+
