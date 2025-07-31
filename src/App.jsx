a452f26c4d19fd7e6526d35abecc68a48126c01b
Irina <weedfox053@gmail.com>
2025-07-31 14:37:04 +0300

    React list of goods

--- src/App.jsx
+++ src/App.jsx
@@ -19,53 +19,68 @@ export const App = () => {
   const [sortGoods, setGoods] = useState([...goodsFromServer]);
   const [active, setActive] = useState('');
   const sort = () => {
- setGoods([...sortGoods].sort((a,b) => a.localeCompare(b)))
- setActive('Sort alphabetically');
- }
+    setGoods([...sortGoods].sort((a, b) => a.localeCompare(b)));
+    setActive('Sort alphabetically');
+  };
 
-const sortByLength = () => {
- setGoods([...sortGoods].sort((a, b) => a.length - b.length))
- setActive('Sort by length')
-}
+  const sortByLength = () => {
+    setGoods([...sortGoods].sort((a, b) => a.length - b.length));
+    setActive('Sort by length');
+  };
 
-const reverse = () => {
-  setGoods([...sortGoods].reverse())
-  setActive('Reverse')
-}
+  const reverse = () => {
+    setGoods([...sortGoods].reverse());
+    setActive('Reverse');
+  };
 
-const reset = () => {
-  setGoods([...goodsFromServer])
-  setActive('')
-}
+  const reset = () => {
+    setGoods([...goodsFromServer]);
+    setActive('');
+  };
 
-return (
-  <div className="section content">
-    <div className="buttons">
-      <button type="button" onClick={sort} className={`button is-info ${active == 'Sort alphabetically' ? '' : 'is-light'}`}>
-        Sort alphabetically
-      </button>
+  return (
+    <div className="section content">
+      <div className="buttons">
+        <button
+          type="button"
+          onClick={sort}
+          className={`button is-info ${active === 'Sort alphabetically' ? '' : 'is-light'}`}
+        >
+          Sort alphabetically
+        </button>
 
-      <button type="button" onClick={sortByLength} className={`button is-success ${active == 'Sort by length' ? '' : 'is-light'}`}>
-        Sort by length
-      </button>
+        <button
+          type="button"
+          onClick={sortByLength}
+          className={`button is-success ${active === 'Sort by length' ? '' : 'is-light'}`}
+        >
+          Sort by length
+        </button>
 
-      <button type="button" onClick={reverse} className={`button is-warning ${active == 'Reverse' ? '' : 'is-light'}`}>
-        Reverse
-      </button>
+        <button
+          type="button"
+          onClick={reverse}
+          className={`button is-warning ${active === 'Reverse' ? '' : 'is-light'}`}
+        >
+          Reverse
+        </button>
 
-      <button type="button"  onClick={reset} className="button is-danger is-light">
-        Reset
-      </button>
+        <button
+          type="button"
+          onClick={reset}
+          className="button is-danger is-light"
+        >
+          Reset
+        </button>
+      </div>
+
+      <ul>
+        {sortGoods.map(good => (
+          <li key={good.id} data-cy="Good">
+            {good.name}
+          </li>
+        ))}
+      </ul>
     </div>
-
-    <ul>
-      {sortGoods.map((good, index) =>
-      <li data-cy="Good"
-      key={index}
-      >{good}</li>
-    )}
-    </ul>
-  </div>
-);
+  );
 };
-
